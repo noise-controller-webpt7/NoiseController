976300960ba892e981e866003a007e524f432473
AidenBow <AidenBow@users.noreply.github.com>
2019-06-21 18:26:01 -0700

    gave the second school blog a data-tab and created TabLink class for tabs.js

--- JS/tabs.js
+++ JS/tabs.js
@@ -1,2 +1,18 @@
-console.log("tabs")
-let tabs = document.querySelectorAll(".tab").forEach(item => console.log(item))
+class TabLink {
+    constructor(tabElement){
+        this.tabElement = tabElement;
+        this.tabData = this.tabElement.dataset.tab;
+        console.log(this.tabData)
+
+        if(this.tabData == "all") {
+            this.blogs = document.querySelectorAll(".blog");
+        } else {
+            this.blogs = document.querySelectorAll(`.blog[data-tab="${this.tabData}"]`) // selects the blogs with the same data-tab
+        }
+
+        this.blogs = Array.from(this.blogs).map(item => console.log(item))
+    }
+}
+
+
+let tabs = document.querySelectorAll(".tab").forEach(item => new TabLink(item))
