d971eb649e9fa54057bd5bb4ce9583b931011b78
AidenBow <AidenBow@users.noreply.github.com>
2019-06-28 19:34:30 -0700

    removed executor function, syntax error was caused by tabs.js being called twice. Removed the extra script tag from blog.html

--- JS/tabs.js
+++ JS/tabs.js
@@ -1,41 +1,37 @@
-function tabExecuter() {
-    class TabLink {
-        constructor(tabElement) {
-            this.tabElement = tabElement;
-            this.tabData = this.tabElement.dataset.tab;
+class TabLink {
+    constructor(tabElement) {
+        this.tabElement = tabElement;
+        this.tabData = this.tabElement.dataset.tab;
 
-            if(this.tabData == "all") {
-                this.blogs = document.querySelectorAll(".blog");
-            } else {
-                this.blogs = document.querySelectorAll(`.blog[data-tab="${this.tabData}"]`)
-            }
-
-            this.blogs = Array.from(this.blogs).map(item => new TabBlog(item))
-            this.tabElement.addEventListener("click", () => this.selectTab());
+        if(this.tabData == "all") {
+            this.blogs = document.querySelectorAll(".blog");
+        } else {
+            this.blogs = document.querySelectorAll(`.blog[data-tab="${this.tabData}"]`)
         }
 
-        selectTab(){
-            const tabs = document.querySelectorAll(".tab");
-            tabs.forEach(tab => tab.classList.remove("active-tab"));
-            const blogs = document.querySelectorAll(".blog");
-            blogs.forEach(blog => blog.style.display = "none")
-            this.tabElement.classList.add("active-tab");
-            this.blogs.forEach(blog => blog.selectBlog());
-        }
+        this.blogs = Array.from(this.blogs).map(item => new TabBlog(item))
+        this.tabElement.addEventListener("click", () => this.selectTab());
     }
 
-    class TabBlog {
-        constructor(blogElement) {
-            this.blogElement = blogElement;
-        }
+    selectTab(){
+        const tabs = document.querySelectorAll(".tab");
+        tabs.forEach(tab => tab.classList.remove("active-tab"));
+        const blogs = document.querySelectorAll(".blog");
+        blogs.forEach(blog => blog.style.display = "none")
+        this.tabElement.classList.add("active-tab");
+        this.blogs.forEach(blog => blog.selectBlog());
+    }
+}
 
-        selectBlog() {
-        this.blogElement.style.display = "block";
-        }
+class TabBlog {
+    constructor(blogElement) {
+        this.blogElement = blogElement;
     }
 
+    selectBlog() {
+    this.blogElement.style.display = "block";
+    }
+}
 
-    var tabs = document.querySelectorAll(".tab").forEach(item => new TabLink(item))
-};
 
-tabExecuter(); // wrapping code with this function prevents syntax error
+var tabs = document.querySelectorAll(".tab").forEach(item => new TabLink(item))
