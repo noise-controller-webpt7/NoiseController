2f42b129bcd684b0b08e12d7c884d9fa6f09d6c8
AidenBow <AidenBow@users.noreply.github.com>
2019-06-28 19:28:37 -0700

    wrapped tabs.js inside of an executor function to fix the syntax error

--- JS/tabs.js
+++ JS/tabs.js
@@ -1,38 +1,41 @@
-class TabLink {
-    constructor(tabElement) {
-        this.tabElement = tabElement;
-        this.tabData = this.tabElement.dataset.tab;
-        console.log(this.tabData)
+function tabExecuter() {
+    class TabLink {
+        constructor(tabElement) {
+            this.tabElement = tabElement;
+            this.tabData = this.tabElement.dataset.tab;
 
-        if(this.tabData == "all") {
-            this.blogs = document.querySelectorAll(".blog");
-        } else {
-            this.blogs = document.querySelectorAll(`.blog[data-tab="${this.tabData}"]`) // selects the blogs with the same data-tab
+            if(this.tabData == "all") {
+                this.blogs = document.querySelectorAll(".blog");
+            } else {
+                this.blogs = document.querySelectorAll(`.blog[data-tab="${this.tabData}"]`)
+            }
+
+            this.blogs = Array.from(this.blogs).map(item => new TabBlog(item))
+            this.tabElement.addEventListener("click", () => this.selectTab());
         }
 
-        this.blogs = Array.from(this.blogs).map(item => new TabBlog(item))
-        this.tabElement.addEventListener("click", () => this.selectTab());
+        selectTab(){
+            const tabs = document.querySelectorAll(".tab");
+            tabs.forEach(tab => tab.classList.remove("active-tab"));
+            const blogs = document.querySelectorAll(".blog");
+            blogs.forEach(blog => blog.style.display = "none")
+            this.tabElement.classList.add("active-tab");
+            this.blogs.forEach(blog => blog.selectBlog());
+        }
     }
 
-    selectTab(){
-        let tabs = document.querySelectorAll(".tab");
-        tabs.forEach(tab => tab.classList.remove("active-tab"));
-        let blogs = document.querySelectorAll(".blog");
-        blogs.forEach(blog => blog.style.display = "none")
-        this.tabElement.classList.add("active-tab");
-        this.blogs.forEach(blog => blog.selectBlog());
-    }
-}
+    class TabBlog {
+        constructor(blogElement) {
+            this.blogElement = blogElement;
+        }
 
-class TabBlog {
-    constructor(blogElement) {
-        this.blogElement = blogElement;
+        selectBlog() {
+        this.blogElement.style.display = "block";
+        }
     }
 
-    selectBlog() {
-    this.blogElement.style.display = "block";
-    }
-}
 
+    var tabs = document.querySelectorAll(".tab").forEach(item => new TabLink(item))
+};
 
-let tabs = document.querySelectorAll(".tab").forEach(item => new TabLink(item))
+tabExecuter(); // wrapping code with this function prevents syntax error
